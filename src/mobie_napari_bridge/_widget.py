f2b0feaedcf545cf27efac8514153e91c0a95b93
Martin Schorb <35071867+martinschorb@users.noreply.github.com>
2024-04-12 16:12:39 +0200

    clean up template widget

--- src/mobie_napari_bridge/_widget.py
+++ src/mobie_napari_bridge/_widget.py
@@ -16,23 +16,6 @@ if TYPE_CHECKING:
 from napari import layers
 
 
-class ExampleQWidget(QWidget):
-    # your QWidget.__init__ can optionally request the napari viewer instance
-    # use a type annotation of 'napari.viewer.Viewer' for any parameter
-    def __init__(self, viewer: "napari.viewer.Viewer"):
-        super().__init__()
-        self.viewer = viewer
-
-        btn = QPushButton("Click me!")
-        btn.clicked.connect(self._on_click)
-
-        self.setLayout(QHBoxLayout())
-        self.layout().addWidget(btn)
-
-    def _on_click(self):
-        print("napari has", len(self.viewer.layers), "layers")
-
-
 class LoadSource(QWidget):
     # your QWidget.__init__ can optionally request the napari viewer instance
     # use a type annotation of 'napari.viewer.Viewer' for any parameter
